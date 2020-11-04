0be18c09d93c54f82bd8383c70461b084c1001f2
MickyJvR <60913251+MickyJvR@users.noreply.github.com>
2020-11-04 12:11:22 +0200

    Test Beat

---
+++ BeatProto URP/Assets/BeatProtoStuff/Scripts/EnvironmentalTesting/BoxTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTest : MonoBehaviour
+{
+    public void OnBeat() 
+    {
+        if (transform.localScale.x == 1)
+        {
+            transform.localScale = new Vector2 (2,2);
+        }
+        else if (transform.localScale.x == 2)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+    }
+}
