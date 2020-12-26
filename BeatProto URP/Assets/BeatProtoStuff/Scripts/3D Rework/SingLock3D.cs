d0be9bf55c6ab66e2d6701c74d63b51160c646ff
MickyJvR <60913251+MickyJvR@users.noreply.github.com>
2020-12-26 07:52:25 +0200

    Converted all the main controls that are not related to beats to 3D. They still need some tweaking (esp the jump). Next step is the beat stuff

---
+++ BeatProto URP/Assets/BeatProtoStuff/Scripts/3D Rework/SingLock3D.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SingLock3D : MonoBehaviour
+{
+
+    public SingLockDoor3D myDoor;
+    private SpriteRenderer mySprite;
+
+    public bool unlocked = false;
+    public bool addedToDoor = false;
+    private bool singCollision = false;
+
+    public float playerMaxDistance;
+    private float playerDistance;
+
+    public float stayUnlockedTime;
+
+    private Text countDownText;
+
+
+    public Color defaultColour;
+    public Color unlockedColor;
+
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        mySprite.color = defaultColour;
+        countDownText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //playerDistance = Vector2.Distance(PlayerController.pController.player.transform.position, transform.position);
+
+        // if (playerDistance < playerMaxDistance)
+        // {
+        //     if (PlayerController.pController.singing)
+        //     {
+        //         stayUnlockedTime = 5f;
+        //         mySprite.color = unlockedColor;
+        //         unlocked = true;
+        //         countDownText.text = "";
+        //     }
+        // }
+        if (!singCollision)
+        {
+            if (!myDoor.doorUnlocked)
+            {
+                if (unlocked)
+                {
+                    if (stayUnlockedTime > 0)
+                    {
+                        stayUnlockedTime -= Time.deltaTime;
+                        countDownText.text = ((int)stayUnlockedTime + 1).ToString();
+                    }
+                    if (stayUnlockedTime <= 0)
+                    {
+                        unlocked = false;
+                        mySprite.color = defaultColour;
+                        countDownText.text = "";
+                    }
+                }
+
+            }
+        }
+
+        if (myDoor.doorUnlocked)
+        {
+            countDownText.text = "";
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "SingCollider")
+        {
+            stayUnlockedTime = 5f;
+            mySprite.color = unlockedColor;
+            unlocked = true;
+            countDownText.text = "";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "SingCollider")
+        {
+            stayUnlockedTime = 5f;
+            countDownText.text = "";
+            singCollision = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "SingCollider")
+        {
+            singCollision = false;
+        }
+    }
+
+}
---
+++ BeatProto URP/Assets/BeatProtoStuff/Scripts/3D Rework/SingLockDoor3D.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class SingLockDoor3D : MonoBehaviour
+{
+
+    public enum unlockType { changeColor, disappear }
+
+    public unlockType myUnlockType;
+
+    public SingLock3D[] myLocks;
+
+    private SpriteRenderer mySprite;
+
+    private int unlockCount = 0;
+
+    public bool doorUnlocked = false;
+
+    bool startFade = false;
+
+    public Color unlockColor;
+
+
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        SetDoorForLocks();
+
+    }
+
+
+    void Update()
+    {
+        if (!doorUnlocked)
+            CheckLocks();
+
+        if (startFade)
+        {
+            Color newCol = mySprite.color;
+            newCol.a -= 0.001f;
+
+            mySprite.color = newCol;
+        }
+    }
+
+    public void CheckLocks()
+    {
+        foreach (SingLock3D singLock in myLocks)
+        {
+            if (singLock.unlocked)
+            {
+                if (!singLock.addedToDoor)
+                {
+                    unlockCount++;
+                    singLock.addedToDoor = true;
+                }
+            }
+            else if (!singLock.unlocked)
+            {
+                if (singLock.addedToDoor)
+                {
+                    unlockCount--;
+                    singLock.addedToDoor = false;
+                }
+            }
+        }
+
+
+        if (unlockCount == myLocks.Length)
+        {
+            doorUnlocked = true;
+        }
+
+        if (doorUnlocked)
+        {
+            if (myUnlockType == unlockType.changeColor)
+            {
+                mySprite.color = unlockColor;
+                GetComponent<BoxCollider>().enabled = false;
+            }
+            else if (myUnlockType == unlockType.disappear)
+            {
+                startFade = true;
+                //gameObject.SetActive(false);
+                GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+
+    public void SetDoorForLocks()
+    {
+        foreach (SingLock3D singLock in myLocks)
+        {
+            singLock.myDoor = this;
+        }
+    }
+
+
+
+
+}
